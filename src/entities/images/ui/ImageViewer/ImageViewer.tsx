96a5bd81db52a93e64ea3632fa60d02333d16806
Emil Novikov <emilnovikov392@gmail.com>
2025-09-15 19:46:31 +0600

    feat: select image

--- src/entities/images/ui/ImageViewer/ImageViewer.tsx
+++ src/entities/images/ui/ImageViewer/ImageViewer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 
 interface IImageViewerProps {
@@ -10,10 +10,24 @@ export const ImageViewer: React.FC<IImageViewerProps> = ({
   photoUrls,
   alt,
 }) => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  function handleClick(url: string) {
+    setSelectedImage(url);
+  }
+
   return (
     <div className={styles.container}>
       {photoUrls.map((url) => (
-        <button className={styles.imgContainer} key={url}>
+        <button
+          className={styles.imgContainer}
+          key={url}
+          onClick={() => handleClick(url)}
+          style={{
+            border:
+              selectedImage === url ? "4px solid green" : "1px solid #fff",
+          }}
+        >
           <img className={styles.img} src={url} alt={alt} />
         </button>
       ))}
