aa1daeb00f63a961f15028fc3b88d7a54fe13881
Emil Novikov <emilnovikov392@gmail.com>
2025-09-23 16:50:25 +0600

    refactor: moderate function

--- src/widgets/ValidateImage/ui/ValidateImage.tsx
+++ src/widgets/ValidateImage/ui/ValidateImage.tsx
@@ -9,6 +9,7 @@ import {
   ImageViewer,
   ITWord,
   updateWord,
+  UpdateWordRequestDTO,
 } from "@/entities/images";
 import styles from "./styles.module.scss";
 import { Button } from "@/shared";
@@ -28,14 +29,6 @@ export const ValidateImage: React.FC = () => {
     handleGetNextWord();
   }, []);
 
-  function reset() {
-    handleGetNextWord();
-    setEditedTranslationEn("");
-    setEditedTranslationRu("");
-    setComment("");
-    setSelectedPhoto("");
-  }
-
   function handleGetNextWord() {
     setIsLoading(true);
     getNextWord()
@@ -52,73 +45,53 @@ export const ValidateImage: React.FC = () => {
       });
   }
 
+  function moderateWord(data: UpdateWordRequestDTO) {
+    if (!word) return;
+
+    setIsModerating(true);
+    updateWord(word.id, {
+      ...data,
+      is_moderated: true,
+      en: editedTranslationEn ? editedTranslationEn : word.en,
+      ru: editedTranslationRu ? editedTranslationRu : word.ru,
+      comment: comment,
+    })
+      .then(() => {
+        handleGetNextWord();
+        setEditedTranslationEn("");
+        setEditedTranslationRu("");
+        setComment("");
+        setSelectedPhoto("");
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsModerating(false);
+      });
+  }
+
   function handleGoNext() {
     if (selectedPhoto === "" && word?.photo_urls.length !== 0) {
       return;
     }
 
-    if (!word) return;
-
-    setIsModerating(true);
-    updateWord(word.id, {
-      is_moderated: true,
+    moderateWord({
       selected_photo: selectedPhoto,
-      en: editedTranslationEn ? editedTranslationEn : word.en,
-      ru: editedTranslationRu ? editedTranslationRu : word.ru,
-      comment: comment,
-    })
-      .then(() => {
-        reset();
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setIsModerating(false);
-      });
+    });
   }
 
   function handleIncorectWord() {
-    if (!word) return;
-    setIsModerating(true);
-    updateWord(word.id, {
-      is_moderated: true,
+    moderateWord({
       selected_photo: "",
-      en: editedTranslationEn ? editedTranslationEn : word.en,
-      ru: editedTranslationRu ? editedTranslationRu : word.ru,
-      comment: comment,
       is_wrong_translation: true,
-    })
-      .then(() => {
-        reset();
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setIsModerating(false);
-      });
+    });
   }
 
   function handleWithoutImage() {
-    if (!word) return;
-    setIsModerating(true);
-    updateWord(word.id, {
-      is_moderated: true,
+    moderateWord({
       selected_photo: "",
-      en: editedTranslationEn ? editedTranslationEn : word.en,
-      ru: editedTranslationRu ? editedTranslationRu : word.ru,
-      comment: comment,
-    })
-      .then(() => {
-        reset();
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setIsModerating(false);
-      });
+    });
   }
 
   return (
